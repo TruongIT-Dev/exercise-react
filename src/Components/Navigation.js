d61c2ad5512f30c3f4855e422c9592079334de66
Truong-Nguyen <dangtruongnguyen2@gmail.com>
2023-06-28 08:34:57 +0700

    React Hook – Context & Effect Hook

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,18 +1,31 @@
-import React, { Component } from "react";
+import React from 'react'
+import { useContext } from 'react'
+import { ThemeContext } from './ThemeContext'
 import '../Style/Navigation.css';
-// export default class Navigation extends Component {
+
 export default function Navigation() {
+    const { theme, toggle, dark } = useContext(ThemeContext)
     return (
-        <div className="nav">
-            <nav>
+        <div>
+            <nav style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
                 <ul>
-                    <li><a className="active" href="#home">Home</a></li>
-                    <li><a href="#new">New</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a className='active' href='#home'>Home</a></li>
+                    <li><a href='#news'>News</a></li>
+                    <li><a href='#about'>About</a></li>
+                    <li><a href='#contact'>Contact</a></li>
                 </ul>
+                <div style={{ position: 'relative' }}>
+                    <a className='switch-mode' href='#' onClick={toggle}
+                        style={{
+                            backgroundColor: theme.backgroundColor,
+                            color: theme.color,
+                            outline: 'none'
+                        }} data-testid="toggle-theme-btn"
+                    >
+                        Switch Nav to {!dark ? 'Dark' : 'Light'} mode
+                    </a>
+                </div>
             </nav>
         </div>
     )
-
 }
